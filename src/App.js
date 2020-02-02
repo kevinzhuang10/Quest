c486a1797158e20181d3dee09084af773825e3d4
Kevin Huang <kevinzhuang10@gmail.com>
2020-02-01 19:03:19 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A fitness platform designed to help you succeed.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="getInTouch">
+          Get In Touch
+        </button>
       </header>
     </div>
   );
